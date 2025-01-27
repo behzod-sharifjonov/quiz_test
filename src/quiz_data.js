96eb91e76f23ccb6fb4a49805fce9dfbed739da7
behzod dev <111036426+behzod-sharifjonov@users.noreply.github.com>
2025-01-27 16:27:06 +0500

    cheenged quetions

--- src/quiz_data.js
+++ src/quiz_data.js
@@ -1,199 +1,310 @@
 export const questions = [
-  {
-    id: 1,
-    question: "Agar 2 + 2 = 4 bo'lsa, unda (2 + 2) * 2 nechchiga teng?",
-    options: ["8", "16", "6", "10"],
-    answer: "Parij",
-  },
-  {
-    id: 2,
-    question:
-      "Ko'prikni ta'mirlashda 3 ishchi 6 kunda ishni tugatadi. Nechta ishchi 3 kunda shu ishni tugatishi mumkin?",
-    options: ["6", "9", "4", "5"],
-    answer: "Mars",
-  },
-  {
-    id: 3,
-    question:
-      "Tugun bilan bir biriga bog'langan ikkita ipni qanday qilib hech qanday asbob-uskunalarsiz qisqartirish mumkin?",
-    options: [
-      "Uni cho'zish kerak",
-      "Tugunni yechish kerak",
-      "Ikkita tugun qo'yish kerak",
-      "Ipni aylantirib bog'lash kerak",
-    ],
-    answer: "Tinch okeani",
-  },
-  {
-    id: 4,
-    question:
-      "6 quti ichida 3 ta olma bor. Har bir qutida nechta olma bo'lishi mumkin?",
-    options: [
-      "Har bir qutida 1 ta",
-      "4 ta qutida hech narsa yo'q",
-      "Ba'zilarida bo'lishi mumkin, ba'zilarida yo'q",
-      "Har bir qutida teng bo'lib",
-    ],
-    answer: "Uilyam Shekspir",
-  },
-  {
-    id: 5,
-    question:
-      "Mening yoshim hozirgi yoshimning yarmidan 5 yil kattaroq. Agar men 10 yoshda bo'lganimda, hozir necha yoshdaman?",
-    options: ["15", "20", "25", "30"],
-    answer: "8",
-  },
-  {
-    id: 6,
-    question:
-      "Agar raqamlar ketma-ketligi 2, 5, 8, 11 bo'lsa, keyingi raqam qaysi bo'ladi?",
-    options: ["13", "14", "15", "16"],
-    answer: "Everest",
-  },
-  {
-    id: 7,
-    question: "Quyidagi variantlardan qaysi biri sizga eng ko'p yoqadi?",
-    options: [
-      "Do'stlarga muammolarni hal qilishda yordam berish.",
-      "Rasm chizish yoki yangi narsa yaratish.",
-      "Fan bilan tajriba qilish va yangiliklarni o'rganish.",
-      "Sonlar bilan ishlash va masalalar yechish.",
-    ],
-    answer: "Oksijen",
-  },
-  {
-    id: 8,
-    question:
-      "Agar 5 xonalik raqamning soni 1 xonadagi raqam bilan bir xil bo'lsa va umumiy qiymati 5 ga teng bo'lsa, bu raqam qaysi?",
-    options: ["11111", "50000", "10500", "10001"],
-    answer: "Proxima Sentauri",
-  },
-  {
-    id: 9,
-    question:
-      "Ikki poyezd 80 km/soat tezlikda bir-biriga qarshi yo'nalishda harakatlanmoqda. Ular o'zaro 160 km masofada joylashgan. Necha soat ichida ular bir-biriga yaqinlashadi?",
-    options: ["1 soat", "2 soat", "4 soat", "0,5 soat"],
-    answer: "Titan arum",
-  },
-  {
-    id: 10,
-    question:
-      "Bir kishi hozirda 45 yoshda, uning o'g'li esa 15 yoshda. Necha yil o'tgach, otaning yoshi o'g'lidan ikki barobar katta bo'ladi?",
-    options: ["5 yil", "10 yil", "15 yil", "20 yil"],
-    answer: "Shveytsariya",
-  },
-  {
-    id: 11,
-    question: "Quyidagi variantlardan qaysi biri sizga eng ko'p yoqadi?",
-    options: [
-      "Do'stlarga muammolarni hal qilishda yordam berish.",
-      "Rasm chizish yoki yangi narsa yaratish.",
-      "Fan bilan tajriba qilish va yangiliklarni o'rganish.",
-      "Sonlar bilan ishlash va masalalar yechish.",
-    ],
-    answer: "Shveytsariya",
-  },
-  {
-    id: 12,
-    question: "Qaysi faoliyat sizga eng ko'p zavq beradi?",
-    options: [
-      "Kitob o'qish va yangi ma'lumotlarni o'rganish.",
-      "Sport bilan shug'ullanish va jismoniy mashqlar.",
-      "Musiqa chalish yoki rasm chizish.",
-      "Odamlar bilan muloqot qilish va ularga yordam berish.",
-    ],
-    answer: "Shveytsariya",
-  },
-  {
-    id: 13,
-    question:
-      "Agar sizga bir loyihani tanlash taklif qilinsa, qaysi birini tanlardingiz?",
-    options: [
-      "O'z blogingizni yaratish va yuritish.",
-      "Bino modelini yaratish yoki mashina loyihalash.",
-      "Kimyo yoki fizika bilan tajriba o'tkazish.",
-      "Maktab bayrami yoki tadbirni tashkil qilish.",
-    ],
-    answer: "Shveytsariya",
-  },
-  {
-    id: 14,
-    question: "Qaysi turdagi vazifalar sizga ko'proq yoqadi?",
-    options: [
-      "Ko'p ma'lumot o'rganishni talab qiladigan vazifalar",
-      "Yangi narsa yaratish yoki ijod qilish bilan bog'liq topshiriqlar.",
-      "Amaliy vazifalar, qurilish yoki tajriba o'tkazish.",
-      "Mantiqiy yoki matematik masalalar.",
-    ],
-    answer: "Shveytsariya",
-  },
-  {
-    id: 15,
-    question: "Katta bo'lganingizda qaysi kasbni tanlamoqchisiz?",
-    options: [
-      "Olim yoki tadqiqotchi.",
-      "Rassom, dizayner yoki me'mor.",
-      "Muhandis yoki dasturchi.",
-      "Shifokor yoki o'qituvchi.",
-    ],
-    answer: "Shveytsariya",
-  },
-  {
-    id: 16,
-    question:
-      "Agar do'stingiz xafa bo'lib, jimgina o'tirib qolsa, siz nima qilasiz?",
-    options: [
-      "E'tibor bermayman, u o'zi o'tib ketadi.",
-      "Do'stim bilan gaplashishga harakat qilaman va nima bo'lganini so'rayman.",
-      "O'zim ham xafa bo'lib qolaman.",
-      " Uni yolg'iz qoldiraman, lekin keyinchalik yordam taklif qilaman.",
-    ],
-    answer: "Shveytsariya",
-  },
-  {
-    id: 17,
-    question:
-      "Biror kishiga qattiq jahlingiz chiqqanda qanday javob qaytarasiz?",
-    options: [
-      "Qattiq baqiraman yoki jahlimni bildiramanga harakat qilaman.",
-      " Boshimni sovitishga harakat qilaman va keyin muloyim gaplashaman.",
-      "Ichimga saqlab yuraman va keyin afsuslanaman.",
-      "Biror narsa qilib, orqaga qaytib, vaziyatni o'ylab ko'raman.",
-    ],
-    answer: "Shveytsariya",
-  },
-  {
-    id: 18,
-    question: "Sinfda kimdir muvaffaqiyatga erishganida qanday his qilasiz?",
-    options: [
-      "Rashk qilaman, chunki men ham xuddi shunday bo'lishni xohlardim.",
-      "Do'stimni tabriklayman va uning yutuqlaridan quvonaman.",
-      "Bu haqda ko'p o'ylamayman.",
-      "O'zimni kuchsiz his qilaman va ko'proq ishlashim kerak deb o'ylayman.",
-    ],
-    answer: "Shveytsariya",
-  },
-  {
-    id: 19,
-    question:
-      "Agar sizni kimdir tanqid qilsa, sizning munosabatingiz qanday bo'ladi?",
-    options: [
-      "O'zimni yomon his qilaman va darhol o'zimni himoya qilishga harakat qilaman.",
-      "Fikrlab ko'raman va agar tanqid o'rinli bo'lsa, qabul qilaman.",
-      "E'tibor bermayman va o'zimni haq deb bilaman.",
-      " Boshqalarning gaplarini o'zimga qattiq olmayman, lekin o'ylab ko'raman.",
-    ],
-    answer: "Shveytsariya",
-  },
-  {
-    id: 20,
-    question: "Biror yangi vaziyatga tushib qolsangiz, qanday harakat qilasiz?",
-    options: [
-      "Qattiq xavotirlanaman va qanday qilib to'g'ri yo'l tutishni bilmayman.",
-      "Xotirjam bo'lib, vaziyatni tahlil qilaman va moslashaman.",
-      "Yordam so'rayman va o'zimni ko'rsatmaslikka harakat qilaman.",
-      "Vaqt o'tishi bilan o'zgarishga harakat qilaman, lekin dastlab qiyinchilikka uchrayman.",
-    ],
-    answer: "Shveytsariya",
-  },
-];
+    {
+      id: 1,
+      question: "Agar 3 + 3 = 6 bo'lsa, unda (3 + 3) * 2 nechchiga teng?",
+      options: ["12", "18", "9", "8"],
+      answer: "12",
+    },
+    {
+      id: 2,
+      question:
+        "5 ishchi 10 kunda ishni tugatadi. 2 ishchi shu ishni necha kunda tugatadi?",
+      options: ["25", "20", "15", "30"],
+      answer: "25",
+    },
+    {
+      id: 3,
+      question:
+        "Ikkita ipni qisqartirish uchun nima qilish kerak?",
+      options: [
+        "Uni yanada cho'zish kerak",
+        "Tugunni ochish kerak",
+        "Ikkita tugun qo'yish kerak",
+        "Iplarni aylantirib bog'lash kerak",
+      ],
+      answer: "Tugunni ochish kerak",
+    },
+    {
+      id: 4,
+      question:
+        "4 quti ichida 2 ta apelsin bor. Har bir qutida nechta apelsin bo'lishi mumkin?",
+      options: [
+        "Har bir qutida 1 ta",
+        "2 qutida hech narsa yo'q",
+        "Ba'zilarida bo'lishi mumkin, ba'zilarida yo'q",
+        "Har bir qutida teng bo'lib",
+      ],
+      answer: "2 qutida hech narsa yo'q",
+    },
+    {
+      id: 5,
+      question:
+        "Mening yoshim hozirgi yoshimning yarmidan 7 yil kattaroq. Agar men 12 yoshda bo'lganimda, hozir necha yoshdaman?",
+      options: ["17", "24", "19", "14"],
+      answer: "17",
+    },
+    {
+      id: 6,
+      question:
+        "Agar raqamlar ketma-ketligi 3, 7, 11, 15 bo'lsa, keyingi raqam qaysi bo'ladi?",
+      options: ["19", "20", "21", "22"],
+      answer: "19",
+    },
+    {
+      id: 7,
+      question: "Quyidagi variantlardan qaysi biri sizga ko'proq yoqadi?",
+      options: [
+        "Do'stlarga masalalarni hal qilishda yordam berish.",
+        "Yangi narsalar yaratish yoki rasm chizish.",
+        "Fan bilan tajriba qilish va kashfiyotlar qilish.",
+        "Sonlar bilan ishlash va matematik muammolar yechish.",
+      ],
+      answer: "Yangi narsalar yaratish yoki rasm chizish.",
+    },
+    {
+      id: 8,
+      question:
+        "Agar 4 xonalik raqamning umumiy qiymati 6 ga teng bo'lsa va sonlar 2 martadan takrorlangan bo'lsa, bu raqam qaysi?",
+      options: ["1112", "6000", "1023", "1002"],
+      answer: "1112",
+    },
+    {
+      id: 9,
+      question:
+        "Ikki velosipedchi bir-biriga qarshi yo'nalishda 30 km/soat tezlikda harakatlanmoqda. Ular o'zaro 60 km masofada joylashgan. Necha soatda ular bir-biriga yetib keladi?",
+      options: ["1 soat", "2 soat", "0,5 soat", "1,5 soat"],
+      answer: "1 soat",
+    },
+    {
+      id: 10,
+      question:
+        "Bir kishi hozirda 50 yoshda, uning qizi esa 20 yoshda. Necha yil o'tgach, otaning yoshi qizidan ikki barobar katta bo'ladi?",
+      options: ["10 yil", "15 yil", "20 yil", "25 yil"],
+      answer: "10 yil",
+    },
+  
+    // Qo'shimcha kreativlik bo'yicha savollar:
+    {
+      id: 11,
+      question: "Agar siz dunyodagi istalgan loyihada qatnashsangiz, qaysi birini tanlardingiz?",
+      options: [
+        "Marsga kosmik missiyada qatnashish.",
+        "Global iqlim o'zgarishiga qarshi tadbirlarni tashkil qilish.",
+        "Yangi texnologiyalar yaratish.",
+        "Butun dunyo bo'ylab sayohat qilish va tajribalar o'rganish.",
+      ],
+      answer: "Marsga kosmik missiyada qatnashish.",
+    },
+    {
+      id: 12,
+      question: "Tasavvur qiling, siz o'zingizning shaxsiy ijodiy atelyeni yaratmoqchisiz. Bu yerda nima qilishni xohlardingiz?",
+      options: [
+        "Suratlar chizish va ularni ko'rgazmada namoyish qilish.",
+        "Musiqiy asboblar yaratish va ulardan foydalanish.",
+        "Ijodiy dasturlar va texnologiyalar ishlab chiqish.",
+        "Yangi retseptlar va taomlar yaratish.",
+      ],
+      answer: "Suratlar chizish va ularni ko'rgazmada namoyish qilish.",
+    },
+    {
+      id: 13,
+      question: "Qaysi ijodiy jarayon sizni ko'proq hayajonlantiradi?",
+      options: [
+        "Yangi g'oyalarni o'ylab topish.",
+        "Badiiy loyihalarni hayotga tadbiq qilish.",
+        "Kichik prototiplar yaratish va sinovdan o'tkazish.",
+        "Ko'p sonli muammo va qiyinchiliklarni yechish.",
+      ],
+      answer: "Yangi g'oyalarni o'ylab topish.",
+    },
+    {
+      id: 14,
+      question: "Agar siz san'at va fanni birlashtiruvchi loyiha ustida ishlasangiz, qaysi birini tanlardingiz?",
+      options: [
+        "Musiqa orqali inson ongi tadqiqotlari.",
+        "Rasm chizish orqali matematik formulalar tushuntirish.",
+        "Tabiiy resurslarni san'at bilan ifoda qilish.",
+        "Texnologiya yordamida ijodiy ob'ektlar yaratish.",
+      ],
+      answer: "Texnologiya yordamida ijodiy ob'ektlar yaratish.",
+    },
+    {
+      id: 15,
+      question: "Agar siz ijodiy raqobatga qo'shilsangiz, qanday turdagi loyiha bilan ishtirok etardingiz?",
+      options: [
+        "Yangi dasturiy ilova yaratish.",
+        "Raqamli san'at asari yaratish.",
+        "Texnologik startap asoschisi bo'lish.",
+        "Innovatsion mahsulot dizayni yaratish.",
+      ],
+      answer: "Yangi dasturiy ilova yaratish.",
+    },
+    {
+      id: 16,
+      question: "Agar sizda cheksiz vaqt va resurslar bo'lsa, qanday ijodiy loyiha boshlashingiz mumkin edi?",
+      options: [
+        "Virtual reallik o'yin yaratish.",
+        "Barcha tillarni o'rgatadigan dastur ishlab chiqish.",
+        "O'z-o'zidan rivojlanadigan sun'iy intellekt yaratish.",
+        "Dunyoni yaxshilash uchun global ijodiy kampaniya o'tkazish.",
+      ],
+      answer: "Virtual reallik o'yin yaratish.",
+    },
+    {
+      id: 17,
+      question: "Qaysi turdagi san'at yoki texnologiya sizni eng ko'p ilhomlantiradi?",
+      options: [
+        "Raqamli san'at va animatsiya.",
+        "Musiqa yaratish va ishlab chiqarish.",
+        "Virtual reallik texnologiyalari.",
+        "Yangi dasturiy ilovalar ishlab chiqish.",
+      ],
+      answer: "Raqamli san'at va animatsiya.",
+    },
+    {
+      id: 18,
+      question: "Qanday qilib yangi narsalarni yaratish jarayonida ilhom topasiz?",
+      options: [
+        "Tabiatdan ilhom olish.",
+        "Boshqa ijodiy odamlar bilan suhbatlashish.",
+        "Yangi texnologiyalarni o'rganish.",
+        "O'tmishdagi mashhur loyihalarni o'rganish.",
+      ],
+      answer: "Tabiatdan ilhom olish.",
+    },
+    {
+      id: 19,
+      question: "Ijodiy jarayonda qaysi vositalar sizga ko'proq yordam beradi?",
+      options: [
+        "Dizayn dasturlari va grafik vositalar.",
+        "Musiqa yaratish dasturlari.",
+        "Koding va texnologik vositalar.",
+        "Qalam va bo'yoq bilan ishlash.",
+      ],
+      answer: "Dizayn dasturlari va grafik vositalar.",
+    },
+    {
+      id: 20,
+      question: "Qaysi ijodiy yo'nalishda ishlashni afzal ko'rasiz?",
+      options: [
+        "Raqamli san'at va dizayn.",
+        "Musiqiy kompozitsiyalar yaratish.",
+        "Innovatsion texnologiyalar ishlab chiqish.",
+        "Noyob mahsulot dizayni va ijodiy mahsulotlar yaratish.",
+      ],
+      answer: "Raqamli san'at va dizayn.",
+    },
+  
+    // Qo'shimcha 10 ta savol:
+    {
+      id: 21,
+      question: "Tasavvur qiling, o'z film studiyangizni ochmoqchisiz. Birinchi filmingiz qaysi janrda bo'ladi?",
+      options: [
+        "Fantastika va ilmiy yutuqlar.",
+        "Drama va emotsional hikoyalar.",
+        "Komediya va kulguli vaziyatlar.",
+        "Sarguzasht va aksiyaga boy hikoyalar.",
+      ],
+      answer: "Fantastika va ilmiy yutuqlar.",
+    },
+    {
+      id: 22,
+      question: "Ijodiy loyihalaringiz uchun qaysi texnologiyani tanlardingiz?",
+      options: [
+        "Sun'iy intellekt va mashina o'rganish.",
+        "3D animatsiya va modelleştirme.",
+        "Virtual haqiqat va augmented haqiqat.",
+        "Veb dasturlash va interfeys dizayni.",
+      ],
+      answer: "Sun'iy intellekt va mashina o'rganish.",
+    },
+    {
+      id: 23,
+      question: "Agar ijodiy g'oyalaringizni aks ettiruvchi shaxsiy logotip yaratish imkoniyatingiz bo'lsa, bu logotip qanday ko'rinishda bo'lardi?",
+      options: [
+        "Geometrik shakllar va minimalistik dizayn.",
+        "Rang-barang va dinamik uslub.",
+        "Klassik va an'anaviy elementlar.",
+        "Abstrakt va murakkab tuzilish.",
+      ],
+      answer: "Geometrik shakllar va minimalistik dizayn.",
+    },
+    {
+      id: 24,
+      question: "Yangi mahsulotni loyihalashda qanday yondashuvni afzal ko'rasiz?",
+      options: [
+        "Innovatsion va texnologik yondashuv.",
+        "Estetik va vizual jozibadorlik.",
+        "Praktik va funksional dizayn.",
+        "Ekologik va barqaror yondashuv.",
+      ],
+      answer: "Innovatsion va texnologik yondashuv.",
+    },
+    {
+      id: 25,
+      question: "Tasavvur qiling, siz bir san'at ko'rgazmasida ishtirok etmoqchisiz. Qaysi turdagi asar yaratishni xohlardingiz?",
+      options: [
+        "Raqamli san'at va multimedia.",
+        "Haykaltaroshlik va 3D asarlar.",
+        "Rangli portretlar va tasvirlar.",
+        "Murakkab grafik dizayn va plakatlar.",
+      ],
+      answer: "Raqamli san'at va multimedia.",
+    },
+    {
+      id: 26,
+      question: "Agar sizga butun dunyo bo'ylab ijodiy seminar o'tkazish imkoniyati berilsa, bu seminarning mavzusi nima bo'lar edi?",
+      options: [
+        "Yangi texnologiyalar yordamida ijod qilish.",
+        "Kreativlik va muammolarni hal qilish usullari.",
+        "San'at va dizaynning kelajagi.",
+        "Musiqiy ijod va texnologik yangiliklar.",
+      ],
+      answer: "Yangi texnologiyalar yordamida ijod qilish.",
+    },
+    {
+      id: 27,
+      question: "Tasavvur qiling, siz ijodiy jamoa bilan ishlayapsiz. Bu jamoada qanday rol o'ynashingiz mumkin?",
+      options: [
+        "Loyiha yetakchisi va strateg.",
+        "Dizayner va vizual loyihachi.",
+        "Texnik va dasturchi.",
+        "Ijodiy konsultant va g'oya generatori.",
+      ],
+      answer: "Ijodiy konsultant va g'oya generatori.",
+    },
+    {
+      id: 28,
+      question: "Agar siz yangi ijodiy startap ochsangiz, bu startap qaysi sohada faoliyat olib boradi?",
+      options: [
+        "O'yinlar va virtual haqiqat.",
+        "Sun'iy intellekt va robototexnika.",
+        "Grafik dizayn va vizual san'at.",
+        "Ta'lim va ijodiy o'rganish vositalari.",
+      ],
+      answer: "O'yinlar va virtual haqiqat.",
+    },
+    {
+      id: 29,
+      question: "Tasavvur qiling, siz ijodiy brend yaratmoqchisiz. Bu brendning asosiy qadriyatlari nima bo'lardi?",
+      options: [
+        "Innovatsiya va texnologik ilg'orlik.",
+        "Barqarorlik va ekologik do'stlik.",
+        "Estetika va vizual mukammallik.",
+        "Soddalik va funksionallik.",
+      ],
+      answer: "Innovatsiya va texnologik ilg'orlik.",
+    },
+    {
+      id: 30,
+      question: "Agar siz ijodiy loyiha uchun jamoa yig'sangiz, qaysi xususiyatlarga ega odamlarni tanlardingiz?",
+      options: [
+        "Innovatorlar va yangilikka ochiq odamlar.",
+        "Analitik va mantiqiy fikrlovchilar.",
+        "San'atkorlar va dizaynerlar.",
+        "Texnik va muhandislar.",
+      ],
+      answer: "Innovatorlar va yangilikka ochiq odamlar.",
+    },
+  ];
+  
